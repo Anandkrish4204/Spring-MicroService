a9a473c25d895f8429ee0deb8766d17235a4bf0b
Anand Krishnamoorthy <anandkrish4204@gmail.com>
2020-04-05 18:23:53 +0530

    Initial Commit

---
+++ src/main/java/anand/microservice/movieservice/resource/MovieInfoResource.java
@@ -0,0 +1,16 @@
+package anand.microservice.movieservice.resource;
+
+import anand.microservice.movieservice.model.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieInfoResource {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovie(@PathVariable("movieId") String movieId){
+        return new Movie(movieId,"test Movie");
+    }
+}
